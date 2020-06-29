d6dd8fe68ca8cecd4291919110ba1c6750f815d2
Yanis AHRAS <a.yanis@hotmail.com>
2020-06-29 09:21:26 +0200

    Ajout partie web et serveur

--- app.py
+++ app.py
@@ -6,9 +6,16 @@ app = Flask(__name__)
 @app.route('/', methods=['POST'])
 #@app.route("/")
 def hello():
-    base64data = request.form['image']
-    resultat =  mainmodel(base64data)
-    return jsonify(resultat)
+
+    try:
+        base64data = request.form['image']
+        resultat =  mainmodel(base64data)
+        print(type(resultat))
+        #return jsonify(resultat)
+        return resultat
+          #flask.make_response(jsonify(resultat),200)
+    except:
+        print('erreur serveuuuuuur <------------------->')
 
 if __name__ == '__main__':
     app.run(debug=True)
